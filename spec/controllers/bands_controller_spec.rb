50ed91516ccf7edc63509fa22cac4c3af99544f9
Jason Williams <jasonmw_nf@hotmail.com>
2015-03-10 16:54:45 -0230

    updated testing

--- spec/controllers/bands_controller_spec.rb
+++ spec/controllers/bands_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe BandsController do 
   describe "GET show" do 
-
+    it "sets @band" do 
+      band = Band.create(name: "Sheavy")
+      get :show, id: band.id
+      expect(assigns(:band)).to eq(band) 
+    end
   end
 end
