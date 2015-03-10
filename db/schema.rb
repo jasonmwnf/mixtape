4d7168400f9cad85312a73f5d4fdc5c1b6452837
Jason Williams <jasonmw_nf@hotmail.com>
2015-03-10 14:47:24 -0230

    added song model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310142109) do
+ActiveRecord::Schema.define(version: 20150310145424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20150310142109) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "genre"
+  end
+
+  create_table "songs", force: true do |t|
+    t.string   "title"
+    t.integer  "band_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,15 @@
 band1 = Band.create(name: 'Sheavy', bio: 'Heavy metal band from Newfoundland, Canada', genre: 'metal')
 band2 = Band.create(name: 'Tool', bio: 'Best band in the world', genre: 'metal')
 band3 = Band.create(name: 'Jon Gomm', bio: 'progressive finger style acoustic god', genre: 'acoustic')
+
+song1 = Song.create(title: 'War on Titan', band_id: band1.id)
+song2 = Song.create(title: 'Beyond the Black Waves', band_id: band1.id)
+song3 = Song.create(title: 'The Rings of Saturn', band_id: band1.id)
+
+song4 = Song.create(title: 'Jambi', band_id: band2.id)
+song5 = Song.create(title: 'Jimmy', band_id: band2.id)
+song6 = Song.create(title: '3rd Eye', band_id: band2.id)
+
+song7 = Song.create(title: 'Passion Flower', band_id: band3.id)
+song8 = Song.create(title: 'Temporary', band_id: band3.id)
+song9 = Song.create(title: 'Weather Machine', band_id: band3.id)
